ba31f63579d846e9359b3a355c5464520076740f
Merlin Ambros <highland88999@gmail.com>
2026-06-28 17:56:20 -0600

    Create package-lock.js

---
+++ home/runner/work/Nexus-Model-Generation/package-lock.js
@@ -0,0 +1,15 @@
+{
+  "name": "olympus-3d-generator",
+  "version": "1.1.0",
+  "lockfileVersion": 3,
+  "requires": true,
+  "packages": {
+    "": {
+      "name": "olympus-3d-generator",
+      "version": "1.1.0",
+      "engines": {
+        "node": ">=18.0.0"
+      }
+    }
+  }
+}
